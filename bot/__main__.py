550dce30af551b1687a40797476a4947df08f443
spotifyman <95912390+spotifyman@users.noreply.github.com>
2021-12-10 15:59:02 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,8 +62,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/mltbbugsreport")
+    buttons.buildbutton("Repo", "https://www.github.com")
+    buttons.buildbutton("Report Group", "https://t.me/cypherhelp_bot")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
